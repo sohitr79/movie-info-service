7205a005ac48e99237e8a3715e9e4c4ce053445d
sohit <sohitr79@gmail.com>
2025-02-01 17:09:08 +0530

    Calling external API to get details of movie

--- src/main/java/com/incubation/movie_info_service/controller/MovieInfoController.java
+++ src/main/java/com/incubation/movie_info_service/controller/MovieInfoController.java
@@ -1,17 +1,46 @@
 package com.incubation.movie_info_service.controller;
 
 import com.incubation.movie_info_service.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieInfoController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${api.bearer_token}")
+    private String BEARER_TOKEN;
+
     @GetMapping("/{movieId}")
-    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
-        return new Movie(movieId,"DDLJ","Love");
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+
+        String url = "https://api.themoviedb.org/3/movie/" + movieId;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(BEARER_TOKEN); // Automatically adds "Bearer <token>"
+
+
+        // Create an entity with headers
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // Make the request with exchange()
+        ResponseEntity<MovieSummary> response = restTemplate.exchange(
+                url, HttpMethod.GET, entity, MovieSummary.class);
+
+        MovieSummary movieSummary = response.getBody();
+
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
     }
 }
---
+++ src/main/java/com/incubation/movie_info_service/controller/MovieSummary.java
@@ -0,0 +1,18 @@
+package com.incubation.movie_info_service.controller;
+
+public class MovieSummary {
+
+    private String id;
+    private String title;
+    private String overview;
+
+    public String getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getOverview() {
+        return overview;
+    }
+}
